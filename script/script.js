3cde96ac9554a6f0c137f3a9905f244146c01824
alessandroNessi <alessandronessi@msn.com>
2021-08-31 16:14:01 +0200

    fixed the function bug

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 function checkPal(word){
-    console.log("checkpal");
     let len=word.length;
     for(let i=0; i<Math.floor(len/2);i++){
         if(word[i]!=word[len-1-i]){
@@ -8,9 +7,7 @@ function checkPal(word){
     }
     return true;
 }
-function writePalResult(){
-    let word=document.getElementById("wordInput").value;
-    console.log("writepal: " + word);
+function writePalResult(word){
     if(word!=""){
         if(checkPal(word)){
             document.getElementById("palRes").innerHTML="True";
@@ -20,9 +17,9 @@ function writePalResult(){
     }
 }
 //event listener part
-document.getElementById("checkPal").addEventListener("click", writePalResult());
-// document.getElementById("wordInput").addEventListener("keydown", function(event){
-//     if(event.key=="Enter"){
-//         writePalResult(document.getElementById("wordInput").value);
-//     }
-// });
+document.getElementById("checkPal").addEventListener("click", function(){writePalResult(document.getElementById("wordInput").value)});
+document.getElementById("wordInput").addEventListener("keydown", function(event){
+    if(event.key=="Enter"){
+        writePalResult(document.getElementById("wordInput").value);
+    }
+});
