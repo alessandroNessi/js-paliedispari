df81961c8cff773b7380344c7eb26f5a1753b154
alessandroNessi <alessandronessi@msn.com>
2021-08-31 16:05:51 +0200

    problem with eventListener

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 function checkPal(word){
+    console.log("checkpal");
     let len=word.length;
     for(let i=0; i<Math.floor(len/2);i++){
         if(word[i]!=word[len-1-i]){
@@ -7,3 +8,21 @@ function checkPal(word){
     }
     return true;
 }
+function writePalResult(){
+    let word=document.getElementById("wordInput").value;
+    console.log("writepal: " + word);
+    if(word!=""){
+        if(checkPal(word)){
+            document.getElementById("palRes").innerHTML="True";
+        }else{
+            document.getElementById("palRes").innerHTML="False";
+        }
+    }
+}
+//event listener part
+document.getElementById("checkPal").addEventListener("click", writePalResult());
+// document.getElementById("wordInput").addEventListener("keydown", function(event){
+//     if(event.key=="Enter"){
+//         writePalResult(document.getElementById("wordInput").value);
+//     }
+// });
